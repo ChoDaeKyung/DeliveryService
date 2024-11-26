ea19d17935488dce53300fde633f8ca01d306e09
ChoDaeKyung <dddodo1004@naver.com>
2024-11-26 17:18:59 +0900

    1718

--- src/main/java/com/example/tobi/selectfront/controller/SelectController.java
+++ src/main/java/com/example/tobi/selectfront/controller/SelectController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SelectController {
 
-    @GetMapping("/select")
-    public String select() {
-        return "select";
+    @GetMapping("/main")
+    public String main() {
+        return "main";
     }
 }
--- src/test/java/com/example/tobi/selectfront/controller/SelectController.java
+++ src/test/java/com/example/tobi/selectfront/controller/SelectController.java
@@ -8,7 +8,7 @@ public class SelectController {
 
     @GetMapping
     public String select() {
-        return "select";
+        return "main";
     }
 
 }
