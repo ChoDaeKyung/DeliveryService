c52e76a6d2c2bbc2e7aaf824b6288ad393d6ec1e
KimSe-hyuk <rlatpgur627@gmail.com>
2024-12-19 17:18:16 +0900

    "final login"

--- src/main/java/com/example/selectfront/client/LoginClient.java
+++ src/main/java/com/example/selectfront/client/LoginClient.java
@@ -1,6 +1,8 @@
 package com.example.selectfront.client;
 
 
+import com.example.selectfront.dto.ClaimsRequestDTO;
+import com.example.selectfront.dto.ClaimsResponseDTO;
 import com.example.selectfront.dto.findMemberResponseDTO;
 import com.example.selectfront.dto.member.*;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -40,4 +42,6 @@ public interface LoginClient {
 
     @PostMapping("/find/user/update-pw")
     findMemberResponseDTO resetPwd(@RequestBody UpdatePwTokenRequestDTO updatePwTokenRequestDTO);
+    @PostMapping("/auths/claims")
+    ClaimsResponseDTO getClaims(@RequestBody ClaimsRequestDTO claimsRequestDTO);
 }
